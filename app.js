ea0068d829292955f0637355aa3d086b8e738c38
Anelise Estevam <aestevam.dev@gmail.com>
2022-07-27 11:57:26 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelectorAll(".links");
+
+navToggle.addEventListener("click", () => {
+  links.forEach((link) => {
+    link.classList.toggle("show-links");
+  });
+});
